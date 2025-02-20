84f3cd1570e8b4d69b49fbbeac51e003164fd85d
Cankat Çağlar Acarer <101988839+CankatCaglar@users.noreply.github.com>
2025-02-20 14:51:42 +0300

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,9 @@
+
+name = input("What is your name? ")
+
+print("Hello " + name + ".")
+
+studentId = input("What is your student ID? ")
+
+print("Your ID is " + studentId + ".")
+
---
+++ Q2.py
@@ -0,0 +1,12 @@
+var1 = int(input("Enter the value for var1: "))
+var2 = int(input("Enter the value for var2: "))
+
+sum = var1 + var2
+diff = var1 - var2
+prod = var1 * var2
+
+print("var1 =", var1)
+print("var2 =", var2)
+print("summation =", sum)
+print("difference =", diff)
+print("multiplication =", prod)
